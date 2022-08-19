e06e00ccb1b5a0018c9f21d1c046c08802dba27c
bglgwyng <bglgwyng@gmail.com>
2022-08-20 00:48:46 +0900

    Disable contour processing.

--- android/src/main/java/com/visioncamerafacedetector/VisionCameraFaceDetectorPlugin.java
+++ android/src/main/java/com/visioncamerafacedetector/VisionCameraFaceDetectorPlugin.java
@@ -136,19 +136,19 @@ public class VisionCameraFaceDetectorPlugin extends FrameProcessorPlugin {
         for (Face face : faces) {
           WritableMap map =  new WritableNativeMap();
 
-          map.putDouble("rollAngle", face.getHeadEulerAngleZ()); // Head is rotated to the left rotZ degrees
-          map.putDouble("pitchAngle", face.getHeadEulerAngleX()); // Head is rotated to the right rotX degrees
-          map.putDouble("yawAngle", face.getHeadEulerAngleY());  // Head is tilted sideways rotY degrees
-          map.putDouble("leftEyeOpenProbability", face.getLeftEyeOpenProbability());
-          map.putDouble("rightEyeOpenProbability", face.getRightEyeOpenProbability());
-          map.putDouble("smilingProbability", face.getSmilingProbability());
+          // map.putDouble("rollAngle", face.getHeadEulerAngleZ()); // Head is rotated to the left rotZ degrees
+          // map.putDouble("pitchAngle", face.getHeadEulerAngleX()); // Head is rotated to the right rotX degrees
+          // map.putDouble("yawAngle", face.getHeadEulerAngleY());  // Head is tilted sideways rotY degrees
+//          map.putDouble("leftEyeOpenProbability", face.getLeftEyeOpenProbability());
+//          map.putDouble("rightEyeOpenProbability", face.getRightEyeOpenProbability());
+//          map.putDouble("smilingProbability", face.getSmilingProbability());
 
 
-          WritableMap contours = processFaceContours(face);
+//          WritableMap contours = processFaceContours(face);
           WritableMap bounds = processBoundingBox(face.getBoundingBox());
 
           map.putMap("bounds", bounds);
-          map.putMap("contours", contours);
+//          map.putMap("contours", contours);
 
           array.pushMap(map);
         }
